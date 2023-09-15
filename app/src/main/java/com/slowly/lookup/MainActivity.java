4bd528e49ed9d77d30edfa96174bb28b71ca1807
Slowlydev <slowly.developers@gmail.com>
2023-09-15 13:11:23 +0200

    feat: more fun

--- app/src/main/java/com/slowly/lookup/MainActivity.java
+++ app/src/main/java/com/slowly/lookup/MainActivity.java
@@ -3,14 +3,25 @@ package com.slowly.lookup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        // TODO do more with progress bar and do the list population wit the locations
     }
 
-
+    private void addLocations() {
+        ListView locations = findViewById(R.id.locations);
+        ArrayAdapter<>
+    }
 }
---
+++ app/src/main/java/com/slowly/lookup/WeatherLocation.java
@@ -0,0 +1,14 @@
+package com.slowly.lookup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_location);
+    }
+}
